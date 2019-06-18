3fbc32fb0b38d4e328f019570e4025461e7e4f8c
yacovm <yacovm@il.ibm.com>
2019-06-18 22:37:45 +0300

    another attempt at fixing CI
    Change-Id: I56801117c7de1a08d89a29af1592c1cec120b9cc
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- scripts/build_checks.sh
+++ scripts/build_checks.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash -xe
-mkdir -p $GOPATH/src/github.com/hyperledger/
-cp -r $GOPATH/src/github.com/SmartBFT-Go/fabric $GOPATH/src/github.com/hyperledger/
 go test -race ./orderer/consensus/smartbft/...
