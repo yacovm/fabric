625f2a5f6de88f4e7ccc0083fa66bee51b8ecd8a
Yoav Tock <tock@il.ibm.com>
2019-06-18 16:10:32 +0300

    Syncronizer implementation skeleton
    A skeleton for the Syncronizer component.
    
    Change-Id: Ib13e707ee9eec0c5a3a5f11fe04e64d3526e3e3e
    Signed-off-by: Yoav Tock <tock@il.ibm.com>

---
+++ orderer/consensus/smartbft/synchronizer.go
@@ -0,0 +1,50 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package smartbft
+
+import (
+	"github.com/SmartBFT-Go/consensus/smartbftprotos"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/orderer/common/localconfig"
+	"github.com/hyperledger/fabric/orderer/consensus"
+)
+
+type Synchronizer struct {
+	support       consensus.ConsenterSupport
+	baseDialer    *cluster.PredicateDialer
+	clusterConfig localconfig.Cluster
+	blockPuller   BlockPuller
+}
+
+func NewSynchronizer(
+	support consensus.ConsenterSupport,
+	baseDialer *cluster.PredicateDialer,
+	clusterConfig localconfig.Cluster) (*Synchronizer, error) {
+
+	puller, err := newBlockPuller(support, baseDialer, clusterConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Synchronizer{
+		support:       support,
+		baseDialer:    baseDialer,
+		clusterConfig: clusterConfig,
+		blockPuller:   puller,
+	}
+	//TODO
+	return s, nil
+}
+
+func (s *Synchronizer) Close() {
+	//TODO
+}
+
+func (s *Synchronizer) Sync() (smartbftprotos.BlockMetadata, uint64) {
+	//TODO
+	return smartbftprotos.BlockMetadata{}, 0
+}
