f65222fd4434d63ec0e0c551c0c321d023238f86
Yoav Tock <tock@il.ibm.com>
2019-06-18 16:10:32 +0300

    Syncronizer implementation skeleton
    A skeleton for the Syncronizer component.
    
    Change-Id: Ib13e707ee9eec0c5a3a5f11fe04e64d3526e3e3e
    Signed-off-by: Yoav Tock <tock@il.ibm.com>

---
+++ orderer/consensus/smartbft/chain.go
@@ -0,0 +1,10 @@
+package smartbft
+
+import "github.com/hyperledger/fabric/protos/common"
+
+// BlockPuller is used to pull blocks from other OSN
+type BlockPuller interface {
+	PullBlock(seq uint64) *common.Block
+	HeightsByEndpoints() (map[string]uint64, error)
+	Close()
+}
