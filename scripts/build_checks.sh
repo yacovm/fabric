f5eb85afe29ed4abd627b555a2aceaff24f60e6e
yacovm <yacovm@il.ibm.com>
2019-06-18 23:24:46 +0300

    added gofmt and goimports checks
    Change-Id: Iaf50fa6e2964b9aff06005a36900dcd38f691ba2
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- scripts/build_checks.sh
+++ scripts/build_checks.sh
@@ -1,2 +1,31 @@
 #!/usr/bin/env bash -xe
+
+go get -u golang.org/x/tools/cmd/goimports
+
+unformatted=$(find . -name "*.go" | grep -v "^./vendor" | grep -v "pb.go" | grep "./orderer" | xargs gofmt -l)
+
+if [[ $unformatted == "" ]];then
+    echo "gofmt checks passed"
+else
+    echo "The following files needs gofmt:"
+    echo "$unformatted"
+    exit 1
+fi
+
+unformatted=$(find . -name "*.go" | grep -v "^./vendor" | grep -v "pb.go" | grep "./orderer" |xargs goimports -l)
+
+if [[ $unformatted == "" ]];then
+    echo "goimports checks passed"
+else
+    echo "The following files needs goimports:"
+    echo "$unformatted"
+    exit 1
+fi
+
+
 go test -race ./orderer/consensus/smartbft/...
+if [[ $? -ne 0 ]];then
+    echo "unit tests failed"
+    exit 1
+fi
+
