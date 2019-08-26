bce1f5093123845dfd813239cb63f4b9705d322c
yacovm <yacovm@il.ibm.com>
2019-08-26 16:47:39 +0300

    Re-vendor library
    Change-Id: I4af29a3008196718b2cd180560cb99adb1484b74
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- vendor/github.com/SmartBFT-Go/consensus/internal/bft/heartbeatmonitor.go
+++ vendor/github.com/SmartBFT-Go/consensus/internal/bft/heartbeatmonitor.go
@@ -14,7 +14,10 @@ import (
 )
 
 const (
-	DefaultHeartbeatTimeout = 60 * time.Second
+	DefaultHeartbeatTimeout      = 60 * time.Second
+	HeartbeatFrequency           = 10 // How much heart beats per timeout
+	Leader                  Role = false
+	Follower                Role = true
 )
 
 //go:generate mockery -dir . -name HeartbeatTimeoutHandler -case underscore -output ./mocks/
@@ -24,57 +27,199 @@ type HeartbeatTimeoutHandler interface {
 	OnHeartbeatTimeout(view uint64, leaderID uint64)
 }
 
+type Role bool
+
+type roleChange struct {
+	view     uint64
+	leaderID uint64
+	follower Role
+}
+
 type HeartbeatMonitor struct {
-	logger     api.Logger
-	hbTimeout  time.Duration
-	hbInterval time.Duration
-	comm       Comm
-	handler    HeartbeatTimeoutHandler
-	mutex      sync.Mutex
-	timer      *time.Timer
-	view       uint64
-	leaderID   uint64
-	follower   bool
+	scheduler     <-chan time.Time
+	inc           chan incMsg
+	stopChan      chan struct{}
+	commandChan   chan roleChange
+	logger        api.Logger
+	hbTimeout     time.Duration
+	comm          Comm
+	handler       HeartbeatTimeoutHandler
+	view          uint64
+	leaderID      uint64
+	follower      Role
+	lastHeartbeat time.Time
+	lastTick      time.Time
+	running       sync.WaitGroup
+	runOnce       sync.Once
+	timedOut      bool
 }
 
 func NewHeartbeatMonitor(
+	scheduler <-chan time.Time,
 	logger api.Logger,
 	heartbeatTimeout time.Duration,
 	comm Comm,
+	handler HeartbeatTimeoutHandler,
 ) *HeartbeatMonitor {
-	if heartbeatTimeout/10 < time.Nanosecond {
-		return nil
-	}
-
 	hm := &HeartbeatMonitor{
-		logger:     logger,
-		hbTimeout:  heartbeatTimeout,
-		hbInterval: heartbeatTimeout / 10,
-		comm:       comm,
+		stopChan:    make(chan struct{}),
+		inc:         make(chan incMsg),
+		commandChan: make(chan roleChange),
+		scheduler:   scheduler,
+		logger:      logger,
+		hbTimeout:   heartbeatTimeout,
+		comm:        comm,
+		handler:     handler,
 	}
 	return hm
 }
 
-func (hm *HeartbeatMonitor) SetTimeoutHandler(handler HeartbeatTimeoutHandler) {
-	// TODO
-}
-
-// StartFollower will start following the heartbeats of the leader of the view.
-func (hm *HeartbeatMonitor) StartFollower(view uint64, leaderID uint64) {
-	// TODO
-}
-
-// StartLeader will start sending heartbeats to all followers.
-func (hm *HeartbeatMonitor) StartLeader(view uint64, leaderID uint64) {
-	// TODO
-}
-
-// ProcessMsg handles an incoming heartbeat.
-func (hm *HeartbeatMonitor) ProcessMsg(sender uint64, msg *smartbftprotos.HeartBeat) {
-	// TODO
+func (hm *HeartbeatMonitor) start() {
+	hm.running.Add(1)
+	go hm.run()
 }
 
 // Close stops following or sending heartbeats.
 func (hm *HeartbeatMonitor) Close() {
-	// TODO
+	if hm.closed() {
+		return
+	}
+
+	defer hm.running.Wait()
+	close(hm.stopChan)
+}
+
+func (hm *HeartbeatMonitor) run() {
+	defer hm.running.Done()
+	for {
+		select {
+		case <-hm.stopChan:
+			return
+		case now := <-hm.scheduler:
+			hm.tick(now)
+		case msg := <-hm.inc:
+			hm.handleMsg(msg.sender, msg.Message)
+		case cmd := <-hm.commandChan:
+			hm.handleCommand(cmd)
+		}
+	}
+}
+
+// ProcessMsg handles an incoming heartbeat.
+// If the sender and msg.View equal what we expect, and the timeout had not expired yet, the timeout is extended.
+func (hm *HeartbeatMonitor) ProcessMsg(sender uint64, msg *smartbftprotos.Message) {
+	hm.inc <- incMsg{
+		sender:  sender,
+		Message: msg,
+	}
+}
+
+// ChangeRole will change the role of this HeartbeatMonitor
+func (hm *HeartbeatMonitor) ChangeRole(follower Role, view uint64, leaderID uint64) {
+	hm.runOnce.Do(func() {
+		hm.follower = follower
+		hm.start()
+	})
+
+	role := "leader"
+	if follower {
+		role = "follower"
+	}
+
+	hm.logger.Infof("Changing to %s role, current view: %d, current leader: %d", role, view, leaderID)
+	hm.commandChan <- roleChange{
+		leaderID: leaderID,
+		view:     view,
+		follower: follower,
+	}
+}
+
+func (hm *HeartbeatMonitor) handleMsg(sender uint64, msg *smartbftprotos.Message) {
+	if !hm.follower {
+		hm.logger.Infof("Heartbeat monitor is not a follower, ignoring; sender: %d, msg: %v", sender, msg)
+		return
+	}
+
+	if sender != hm.leaderID {
+		hm.logger.Infof("Heartbeat from %d which is not the leader %d", sender, hm.leaderID)
+		return
+	}
+
+	hbMsg := msg.GetHeartBeat()
+	// TODO Optimisation: check for extension due to data messages sent by leader (backlog)
+	if hbMsg == nil {
+		return
+	}
+
+	if hbMsg.View != hm.view {
+		hm.logger.Infof("Heartbeat view is different than monitor view, ignoring; view: %d, sender: %d, msg: %v", hm.leaderID, sender, msg)
+		return
+	}
+
+	hm.logger.Debugf("Received heartbeat from %d, last heartbeat was %v ago", sender, hm.lastTick.Sub(hm.lastHeartbeat))
+	hm.lastHeartbeat = hm.lastTick
+}
+
+func (hm *HeartbeatMonitor) tick(now time.Time) {
+	hm.lastTick = now
+	if hm.lastHeartbeat.IsZero() {
+		hm.lastHeartbeat = now
+	}
+	if hm.follower {
+		hm.followerTick(now)
+	} else {
+		hm.leaderTick(now)
+	}
+}
+
+func (hm *HeartbeatMonitor) closed() bool {
+	select {
+	case <-hm.stopChan:
+		return true
+	default:
+		return false
+	}
+}
+
+func (hm *HeartbeatMonitor) handleCommand(cmd roleChange) {
+	hm.timedOut = false
+	hm.view = cmd.view
+	hm.leaderID = cmd.leaderID
+	hm.follower = cmd.follower
+	hm.lastHeartbeat = hm.lastTick
+}
+
+func (hm *HeartbeatMonitor) leaderTick(now time.Time) {
+	if now.Sub(hm.lastHeartbeat)*HeartbeatFrequency < hm.hbTimeout {
+		return
+	}
+
+	hm.logger.Debugf("Sending heartbeat with view %d", hm.view)
+	heartbeat := &smartbftprotos.Message{
+		Content: &smartbftprotos.Message_HeartBeat{
+			HeartBeat: &smartbftprotos.HeartBeat{
+				View: hm.view,
+			},
+		},
+	}
+	hm.comm.BroadcastConsensus(heartbeat)
+	hm.lastHeartbeat = now
+}
+
+func (hm *HeartbeatMonitor) followerTick(now time.Time) {
+	if hm.timedOut || hm.lastHeartbeat.IsZero() {
+		hm.lastHeartbeat = now
+		return
+	}
+
+	delta := now.Sub(hm.lastHeartbeat)
+	if delta >= hm.hbTimeout {
+		hm.logger.Warnf("Heartbeat timeout (%v) from %d expired; last heartbeat was observed %s ago",
+			hm.hbTimeout, hm.leaderID, delta)
+		hm.handler.OnHeartbeatTimeout(hm.view, hm.leaderID)
+		hm.timedOut = true
+		return
+	}
+
+	hm.logger.Debugf("Last heartbeat from %d was %v ago", hm.leaderID, delta)
 }
