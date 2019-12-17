cad0fdf18199ac414768394972533cd3a7d54a89
yacovm <yacovm@il.ibm.com>
2019-12-17 16:35:09 +0200

    publish to dockerhub
    Change-Id: If0f8f6d076fe3eabbb1f4b3d83ff4d0ad570dcf6
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- scripts/publish_images.sh
+++ scripts/publish_images.sh
@@ -7,3 +7,15 @@ git checkout origin/release-1.4-BFT
 
 make orderer-docker peer-docker
 
+echo "Tagging images..."
+docker tag hyperledger/fabric-peer:latest smartbft/fabric-peer:latest
+docker tag hyperledger/fabric-orderer:latest smartbft/fabric-orderer:latest
+
+echo "Logging in..."
+docker login -u smartbft -p ${DOCKER_PASSWORD}
+
+echo "Pushing to dockerhub"
+
+docker push smartbft/fabric-peer:latest
+docker push smartbft/fabric-orderer:latest
+
