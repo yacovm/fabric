b53dadb65fe375f8b4ee82e7db6e8cb67a3ff38c
yacovm <yacovm@il.ibm.com>
2019-12-17 16:25:51 +0200

    experiment with secret
    Change-Id: I00fca8c2cebca2d8628465b06030bfe6caeb6901
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- scripts/publish_images.sh
+++ scripts/publish_images.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash -e
 
 echo "Building docker images..."
+echo "DOCKER_PASSWORD=${DOCKER_PASSWORD}"
+sleep 5
 git checkout origin/release-1.4-BFT
 
 make orderer-docker peer-docker
