819c364a72c9afcfed3dd7cfefdcdb8a8f7b56a7
Gal Assa <galassa@campus.technion.ac.il>
2020-08-12 16:53:21 +0300

    validate block, fixed license

--- gdpr/validate_block.go
+++ gdpr/validate_block.go
@@ -1,28 +1,33 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package gdpr
 
 import (
 	"errors"
 	"fmt"
+
 	"github.com/golang/protobuf/proto"
 
-	//"crypto"
 	"crypto/sha256"
+
 	"github.com/hyperledger/fabric-protos-go/common"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/rwsetutil"
 	"github.com/hyperledger/fabric/protoutil"
-	//"hash"
 )
 
 var ErrVal = errors.New("gdpr: block validation failed")
 
-func getHash(preimage []byte) []byte{
+func getHash(preimage []byte) []byte {
 	h := sha256.New()
 	return h.Sum(preimage)
 
 }
 
-
-func validate (block *common.Block) (*common.Block , error){
+func validate(block *common.Block) (*common.Block, error) {
 	preImages := block.GetData().GetPreimageSpace()
 
 	m := map[string]struct{}{}
@@ -32,13 +37,13 @@ func validate (block *common.Block) (*common.Block , error){
 		m[(string(temp))] = struct{}{}
 	}
 
-	for _ , envBytes := range block.Data.Data {
+	for _, envBytes := range block.Data.Data {
 		env, err := protoutil.GetEnvelopeFromBlock(envBytes)
 		if err != nil {
 			return nil, err
 		}
 
-		payload, err := protoutil.UnmarshalPayload(env.Payload) //protoutil.GetBytesPayload()
+		payload, err := protoutil.UnmarshalPayload(env.Payload)
 		if err != nil {
 			return nil, err
 		}
@@ -54,23 +59,20 @@ func validate (block *common.Block) (*common.Block , error){
 		}
 
 		txRWSet := &rwsetutil.TxRwSet{}
-		//txRWSet.FromProtoBytes(respPayload.Results)
 
 		if err = txRWSet.FromProtoBytes(respPayload.Results); err != nil {
 			return nil, err
 		}
 
 		for _, nsRWSet := range txRWSet.NsRwSets {
-			//nsRWSet.KvRwSet
 			for _, kvWrite := range nsRWSet.KvRwSet.Writes {
 				var a = kvWrite.GetValueHash()
-				if a != nil{
+				if a != nil {
 					fmt.Printf("that's odd")
 				}
 				var b = getHash(kvWrite.GetValue())
-				//kvWrite.ValueHash = []byte("Gal")
-				if memberOf((string)(b),m) == false{
-					return nil,  ErrVal
+				if memberOf((string)(b), m) == false {
+					return nil, ErrVal
 				}
 
 			}
@@ -79,20 +81,15 @@ func validate (block *common.Block) (*common.Block , error){
 	}
 
 	return getVanillaBlock(block)
-
-	//return nil, nil
 }
 
-
-
-
 func memberOf(a string, m map[string]struct{}) bool {
-	_ , exists := m[a]
+	_, exists := m[a]
 	return exists
 
 }
 
-func getVanillaBlock(block *common.Block) (*common.Block , error) {
+func getVanillaBlock(block *common.Block) (*common.Block, error) {
 	newBlock := proto.Clone(block).(*common.Block)
 	newBlock.Data.PreimageSpace = nil
 
@@ -103,35 +100,32 @@ func getVanillaBlock(block *common.Block) (*common.Block , error) {
 	for _, envBytes := range newBlock.Data.Data {
 		env, err := protoutil.GetEnvelopeFromBlock(envBytes)
 		if err != nil {
-			//logger.Warning("Invalid envelope:", err)
-			return nil,err
+			return nil, err
 		}
 
-		payload, err := protoutil.UnmarshalPayload(env.Payload) //protoutil.GetBytesPayload()
+		payload, err := protoutil.UnmarshalPayload(env.Payload)
 		if err != nil {
-			//logger.Warning("Invalid payload:", err)
-			return nil,err
+			return nil, err
 		}
 
 		tx, err := protoutil.UnmarshalTransaction(payload.Data)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 
 		_, respPayload, err := protoutil.GetPayloads(tx.Actions[0])
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 
 		txRWSet := &rwsetutil.TxRwSet{}
 
 		if err = txRWSet.FromProtoBytes(respPayload.Results); err != nil {
-			return nil,err
+			return nil, err
 		}
 		for _, nsRWSet := range txRWSet.NsRwSets {
 			for _, kvWrite := range nsRWSet.KvRwSet.Writes {
 				var a = kvWrite.GetValueHash()
-				//TODO: kvWrite.ValueHash := nil ??
 				temp := getKVWrite(preImages, a)
 				if temp != nil {
 					kvWrite.Value = temp
@@ -141,22 +135,20 @@ func getVanillaBlock(block *common.Block) (*common.Block , error) {
 		}
 	}
 
-	return newBlock,nil
-
+	return newBlock, nil
 
 }
 
-func getKVWrite(preimages [][]byte , hashVal []byte ) []byte {
+func getKVWrite(preimages [][]byte, hashVal []byte) []byte {
 	strVal := (string)(hashVal)
 	for _, pi := range preimages {
 		temp := (string)(getHash(pi))
-		if temp == strVal{
+		if temp == strVal {
 			return pi
 		}
 	}
 	return nil
 }
-
 
 func extractPreimages(block *common.Block) [][]byte {
 
@@ -165,13 +157,11 @@ func extractPreimages(block *common.Block) [][]byte {
 	for _, envBytes := range block.Data.Data {
 		env, err := protoutil.GetEnvelopeFromBlock(envBytes)
 		if err != nil {
-			//logger.Warning("Invalid envelope:", err)
 			continue
 		}
 
-		payload, err := protoutil.UnmarshalPayload(env.Payload) //protoutil.GetBytesPayload()
+		payload, err := protoutil.UnmarshalPayload(env.Payload)
 		if err != nil {
-			//logger.Warning("Invalid payload:", err)
 			continue
 		}
 
@@ -191,7 +181,6 @@ func extractPreimages(block *common.Block) [][]byte {
 			return nil
 		}
 		for _, nsRWSet := range txRWSet.NsRwSets {
-			//nsRWSet.KvRwSet
 			for _, kvWrite := range nsRWSet.KvRwSet.Writes {
 				var a = kvWrite.GetValue()
 				preimages = append(preimages, a)
@@ -204,19 +193,15 @@ func extractPreimages(block *common.Block) [][]byte {
 	return preimages
 }
 
-
-func clearKVWrites(block *common.Block){
-	//clone := proto.Clone(block).(*common.Block)
+func clearKVWrites(block *common.Block) {
 	for _, envBytes := range block.Data.Data {
 		env, err := protoutil.GetEnvelopeFromBlock(envBytes)
 		if err != nil {
-			//logger.Warning("Invalid envelope:", err)
 			return
 		}
 
-		payload, err := protoutil.UnmarshalPayload(env.Payload) //protoutil.GetBytesPayload()
+		payload, err := protoutil.UnmarshalPayload(env.Payload)
 		if err != nil {
-			//logger.Warning("Invalid payload:", err)
 			return
 		}
 
@@ -233,9 +218,9 @@ func clearKVWrites(block *common.Block){
 		txRWSet := &rwsetutil.TxRwSet{}
 
 		if err = txRWSet.FromProtoBytes(respPayload.Results); err != nil {
-			return		}
+			return
+		}
 		for _, nsRWSet := range txRWSet.NsRwSets {
-			//nsRWSet.KvRwSet
 			for _, kvWrite := range nsRWSet.KvRwSet.Writes {
 				kvWrite.Value = nil
 			}
@@ -243,5 +228,3 @@ func clearKVWrites(block *common.Block){
 	}
 
 }
-
-
