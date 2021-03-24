6ca31b83566a4465aac7beee3b5e0f632cdceb29
Hagar Meir <hagar.meir@ibm.com>
2021-03-24 10:48:34 +0200

    heartbeat monitor
    Change-Id: Icaed550abdac5de7847b7a653504d8eaf01e208a
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

---
+++ orderer/consensus/smartbft/heartbeatmonitor_test.go
@@ -0,0 +1,105 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package smartbft_test
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabric/orderer/consensus/smartbft"
+	"github.com/hyperledger/fabric/orderer/consensus/smartbft/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	heartbeatTimeout  = 60 * time.Second
+	heartbeatCount    = 10
+	tickIncrementUnit = heartbeatTimeout / heartbeatCount
+)
+
+func TestNewHeartbeatMonitor(t *testing.T) {
+	rpc := &mocks.RPC{}
+	scheduler := make(chan time.Time)
+	logger := flogging.MustGetLogger("test")
+	hm := smartbft.NewHeartbeatMonitor(rpc, scheduler, logger, heartbeatTimeout, heartbeatCount, smartbft.HeartbeatReceiver, []uint64{1, 2, 3}, []uint64{11, 12, 13})
+	assert.NotNil(t, hm)
+	hm.Start()
+	hm.Close()
+}
+
+func TestHeartbeatMonitorSender(t *testing.T) {
+	rpc := &mocks.RPC{}
+	var sendWG sync.WaitGroup
+	sendWG.Add(3)
+	rpc.On("SendConsensus", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		sendWG.Done()
+	}).Return(nil)
+	scheduler := make(chan time.Time)
+	logger := flogging.MustGetLogger("test")
+	hm := smartbft.NewHeartbeatMonitor(rpc, scheduler, logger, heartbeatTimeout, heartbeatCount, smartbft.HeartbeatSender, []uint64{1, 2, 3}, []uint64{11, 12, 13})
+	assert.NotNil(t, hm)
+	hm.Start()
+	rpc.AssertNotCalled(t, "SendConsensus", mock.Anything, mock.Anything)
+	clock := fakeTime{}
+	clock.advanceTime(2, scheduler)
+	sendWG.Wait()
+	rpc.AssertNumberOfCalls(t, "SendConsensus", 3)
+	sendWG.Add(3)
+	clock.advanceTime(1, scheduler)
+	sendWG.Wait()
+	rpc.AssertNumberOfCalls(t, "SendConsensus", 6)
+	hm.Close()
+}
+
+func TestHeartbeatMonitorReceiver(t *testing.T) {
+	rpc := &mocks.RPC{}
+	var sendWG sync.WaitGroup
+	sendWG.Add(3)
+	scheduler := make(chan time.Time)
+	logger := flogging.MustGetLogger("test")
+	hm := smartbft.NewHeartbeatMonitor(rpc, scheduler, logger, heartbeatTimeout, heartbeatCount, smartbft.HeartbeatReceiver, []uint64{1, 2, 3}, []uint64{11, 12, 13})
+	assert.NotNil(t, hm)
+	hm.Start()
+	assert.Equal(t, []uint64{1, 2, 3}, hm.GetSuspects()) // nobody sent any heartbeats yet and therefore all are suspects
+	clock := fakeTime{}
+	clock.advanceTime(1, scheduler)
+	hm.ProcessHeartbeat(1)
+	hm.ProcessHeartbeat(2)
+	hm.ProcessHeartbeat(3)
+	clock.advanceTime(1, scheduler)
+	assert.Equal(t, []uint64{}, hm.GetSuspects())
+	clock.advanceTime(10, scheduler)
+	assert.Equal(t, []uint64{1, 2, 3}, hm.GetSuspects())
+	clock.advanceTime(5, scheduler)
+	hm.ProcessHeartbeat(2)
+	clock.advanceTime(5, scheduler)
+	assert.Equal(t, []uint64{1, 3}, hm.GetSuspects())
+	clock.advanceTime(5, scheduler)
+	hm.ProcessHeartbeat(1)
+	hm.ProcessHeartbeat(3)
+	clock.advanceTime(5, scheduler)
+	assert.Equal(t, []uint64{2}, hm.GetSuspects())
+	hm.Close()
+}
+
+type fakeTime struct {
+	time time.Time
+}
+
+// each tick is (heartbeatTimeout / heartbeatCount)
+func (t *fakeTime) advanceTime(ticks int, schedulers ...chan time.Time) {
+	for i := 1; i <= ticks; i++ {
+		newTime := t.time.Add(tickIncrementUnit)
+		for _, scheduler := range schedulers {
+			scheduler <- newTime
+		}
+		t.time = newTime
+	}
+}
