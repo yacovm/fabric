d4efec320cc1c6d211f4bbeb2f06481198132f33
Artem Barger <bartem@il.ibm.com>
2021-04-29 15:10:15 +0300

    Non-Committee ledger synchronization

---
+++ orderer/consensus/smartbft/deliver.go
@@ -0,0 +1,246 @@
+package smartbft
+
+import (
+	"bytes"
+	"encoding/hex"
+	"math"
+	"sync/atomic"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/hyperledger/fabric/common/crypto"
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabric/common/util"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/orderer"
+	"github.com/hyperledger/fabric/protos/utils"
+	"google.golang.org/grpc"
+)
+
+// BlocksStreamPuller fetches stream of blocks from active committee members
+type BlocksStreamPuller struct {
+	MaxPullBlockRetries uint64
+	RetryTimeout        time.Duration
+	FetchTimeout        time.Duration
+
+	Endpoints []cluster.EndpointCriteria
+	LastBlock *common.Block
+	TLSCert   []byte
+	Channel   string
+	Signer    crypto.LocalSigner
+
+	Logger *flogging.FabricLogger
+
+	BlockVerifier BlockVerifier
+	Dialer        cluster.Dialer
+	Ledger        LedgerWriter
+	StreamCreator StreamCreator
+
+	deliverEndpointIdx   int
+	blockDeliverEndpoint cluster.EndpointCriteria
+	conn                 *grpc.ClientConn
+	stream               ImpatientStream
+	stopFlag             int32
+}
+
+//go:generate mockery --name=AtomicBroadcastServer --case=underscore --output=mocks/
+type AtomicBroadcastServer interface {
+	orderer.AtomicBroadcastServer
+}
+
+//go:generate mockery --name=ImpatientStream --case=underscore --output=mocks/
+type ImpatientStream interface {
+	orderer.AtomicBroadcast_DeliverClient
+	Abort()
+}
+
+//go:generate mockery --name=BlockVerifier --case=underscore --output=mocks/
+type BlockVerifier interface {
+	cluster.BlockVerifier
+}
+
+//go:generate mockery --name=LedgerWriter --case=underscore --output=mocks/
+type LedgerWriter interface {
+	cluster.LedgerWriter
+}
+
+type StreamCreator func(conn *grpc.ClientConn, timeout time.Duration) (ImpatientStream, error)
+
+// NewImpatientStream creates an instance of the cluster impatient stream
+func NewImpatientStream(conn *grpc.ClientConn, timeout time.Duration) (ImpatientStream, error) {
+	streamCreator := cluster.NewImpatientStream(conn, timeout)
+	return streamCreator()
+}
+
+func (p *BlocksStreamPuller) ContinuouslyPullBlocks() error {
+	retriesCnt := p.MaxPullBlockRetries
+	for !p.isdone() {
+		block, err := p.tryFetchBlocks()
+		if err != nil {
+			if retriesCnt == 0 {
+				p.Logger.Errorf("Failed pulling next block: retry count exhausted(%d), reason %s", p.MaxPullBlockRetries, err)
+				return err
+			}
+			retriesCnt--
+			p.Logger.Errorf("Failed to pull next block: retries left (%d), reason %s", retriesCnt, err)
+			time.Sleep(p.RetryTimeout)
+			p.disconnect()
+			continue
+		}
+
+		p.Logger.Debugf("Appending block (%d) to the ledger", block.Header.Number)
+		if err := p.Ledger.Append(block); err != nil {
+			p.Logger.Errorf("not able to append newly received block into the ledger, block number [%d], due to %s", block.Header.Number, err)
+			return err
+		}
+		p.LastBlock = block
+	}
+	return nil
+}
+
+// Stop halts blocks stream puller from fetching blocks
+func (p *BlocksStreamPuller) Stop() {
+	atomic.StoreInt32(&p.stopFlag, 1)
+	p.disconnect()
+}
+
+// assignEndpoints assigns endpoints to decide where to fetch blocks from
+func (p *BlocksStreamPuller) assignEndpoints() {
+	p.deliverEndpointIdx = (p.deliverEndpointIdx + 1) % len(p.Endpoints)
+	p.blockDeliverEndpoint = p.Endpoints[p.deliverEndpointIdx]
+}
+
+func (p *BlocksStreamPuller) tryFetchBlocks() (*common.Block, error) {
+	if p.disconnected() {
+		if err := p.obtainStream(); err != nil {
+			p.Logger.Errorf("failed to obtain grpc stream to read blocks, because of %s", err)
+			return nil, err
+		}
+		err := p.requestBlocks()
+		if err != nil {
+			p.Logger.Errorf("failed sending seek envelope, error %s, endpoint %s", err, p.blockDeliverEndpoint)
+			return nil, err
+		}
+	}
+	resp, err := p.stream.Recv()
+	if err != nil {
+		p.Logger.Errorf("Failed receiving next block from %s: %v", p.blockDeliverEndpoint, err)
+		return nil, errors.Errorf("failed to receive next block, due to %s", err)
+	}
+
+	block, err := cluster.ExtractBlockFromResponse(resp)
+	if err != nil {
+		p.Logger.Errorf("Received a bad block from %s: %v", p.blockDeliverEndpoint, err)
+		return nil, err
+	}
+
+	if p.LastBlock.Header.Number+1 != block.Header.Number {
+		return nil, errors.Errorf("got unexpected sequence from %s - (%d) instead of (%d)", p.blockDeliverEndpoint, block.Header.Number, p.LastBlock.Header.Number+1)
+	}
+	if !bytes.Equal(block.Header.PreviousHash, p.LastBlock.Header.Hash()) {
+		claimedPrevHash := hex.EncodeToString(block.Header.PreviousHash)
+		actualPrevHash := hex.EncodeToString(p.LastBlock.Header.Hash())
+		return nil,
+			errors.Errorf("block [%d]'s hash (%s) mismatches %d's prev block hash (%s)",
+				p.LastBlock.Header.Number, actualPrevHash, block.Header.Number, claimedPrevHash)
+	}
+
+	// sending nil for config envelope parameter, it will make use of the recent active configuration
+	if err := cluster.VerifyBlockSignature(block, p.BlockVerifier, nil); err != nil {
+		return nil, err
+	}
+	return block, nil
+}
+
+func (p *BlocksStreamPuller) isdone() bool {
+	return atomic.LoadInt32(&p.stopFlag) == 1
+}
+
+// disconnect makes the BlockPuller close the connection and stream
+// with the remote endpoint, and wipe the internal block buffer.
+func (p *BlocksStreamPuller) disconnect() {
+	if p.stream != nil {
+		p.stream.Abort()
+	}
+
+	if p.conn != nil {
+		p.conn.Close()
+	}
+	p.conn = nil
+}
+
+func (p *BlocksStreamPuller) openStream() error {
+	stream, err := p.StreamCreator(p.conn, p.FetchTimeout)
+	if err != nil {
+		p.Logger.Errorf("failed to create impatient delivery stream to fetch blocks from committee, error %s", err)
+		return err
+	}
+	p.stream = stream
+	return nil
+}
+
+func (p *BlocksStreamPuller) obtainStream() error {
+	reConnected := false
+	for p.disconnected() {
+		reConnected = true
+		// not connected, need to select next point and reconnect to it
+		// to continue fetching blocks
+		p.assignEndpoints()
+		// make sure to get connected to committee OSN
+		if err := p.connectToNextEndpoint(); err != nil {
+			time.Sleep(p.RetryTimeout)
+		}
+	}
+
+	if reConnected {
+		err := p.openStream()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *BlocksStreamPuller) requestBlocks() error {
+	if p.stream == nil {
+		return errors.Errorf("grps stream is not initialized, cannot pull blocks from committee")
+	}
+	env, err := utils.CreateSignedEnvelopeWithTLSBinding(
+		common.HeaderType_DELIVER_SEEK_INFO,
+		p.Channel,
+		p.Signer,
+		&orderer.SeekInfo{
+			Start:         &orderer.SeekPosition{Type: &orderer.SeekPosition_Specified{Specified: &orderer.SeekSpecified{Number: p.LastBlock.Header.Number + 1}}},
+			Stop:          &orderer.SeekPosition{Type: &orderer.SeekPosition_Specified{Specified: &orderer.SeekSpecified{Number: math.MaxUint64}}},
+			Behavior:      orderer.SeekInfo_BLOCK_UNTIL_READY,
+			ErrorResponse: orderer.SeekInfo_BEST_EFFORT,
+		},
+		int32(0),
+		uint64(0),
+		util.ComputeSHA256(p.TLSCert),
+	)
+	if err != nil {
+		p.Logger.Errorf("failed to create seek envelope to start fetching blocks, endpoint %s", err, p.blockDeliverEndpoint)
+		return err
+	}
+	// sending request to pull blocks
+	p.Logger.Debugf("sending request to pull blocks from committee, endpoint is [%s]", p.blockDeliverEndpoint)
+	return p.stream.Send(env)
+}
+
+func (p *BlocksStreamPuller) connectToNextEndpoint() error {
+	var err error
+	p.conn, err = p.Dialer.Dial(p.blockDeliverEndpoint)
+	if err != nil {
+		p.Logger.Warningf("Failed connecting to %s: %v", p.blockDeliverEndpoint, err)
+		return err
+	}
+	return nil
+}
+
+// disconnected check whenever puller got already connected
+func (p *BlocksStreamPuller) disconnected() bool {
+	return p.conn == nil
+}
---
+++ orderer/consensus/smartbft/deliver_test.go
@@ -0,0 +1,177 @@
+package smartbft
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/fabric/common/util"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/protos/utils"
+
+	"github.com/hyperledger/fabric/common/mocks/crypto"
+
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabric/core/comm"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/orderer/consensus/smartbft/mocks"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/orderer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc"
+)
+
+type testDialer struct {
+}
+
+func (t *testDialer) Dial(endpointCriteria cluster.EndpointCriteria) (*grpc.ClientConn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*100)
+	defer cancel()
+
+	return grpc.DialContext(ctx, endpointCriteria.Endpoint, grpc.WithBlock(), grpc.WithInsecure())
+}
+
+func TestBlocksStreamPuller_ContinuouslyPullBlocks(t *testing.T) {
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
+	assert.NoError(t, err)
+	defer lis.Close()
+
+	srv, err := comm.NewGRPCServerFromListener(lis, comm.ServerConfig{})
+	assert.NoError(t, err)
+
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+
+	channelID := "test_channel"
+	signerID := uint64(1)
+	signerIdentity := []byte{1, 2, 3, 4}
+	var signature []byte
+	var tlsCert []byte
+
+	lastBlock := common.NewBlock(1, nil)
+	blockResponses := prepareBlocks(lastBlock, []*common.MetadataSignature{
+		{
+			SignerId:  signerID,
+			Signature: signature,
+		},
+	}, 11)
+
+	blockVerifier := &mocks.BlockVerifier{}
+	blockVerifier.On("Id2Identity", mock.Anything).Return(map[uint64][]byte{
+		signerID: signerIdentity,
+	})
+
+	signer := &crypto.LocalSigner{}
+
+	blockVerifier.On("VerifyBlockSignature", mock.Anything, mock.Anything).Run(
+		func(args mock.Arguments) {
+			signatureSet, ok := args.Get(0).([]*common.SignedData)
+			assert.True(t, ok)
+			assert.NotNil(t, signatureSet)
+			assert.Len(t, signatureSet, 1)
+			signedData := signatureSet[0]
+			assert.NotNil(t, signedData)
+			assert.Equal(t, signerIdentity, signedData.Identity)
+		}).Return(nil)
+
+	ledger := &mocks.LedgerWriter{}
+
+	broadcastServer := &mocks.AtomicBroadcastServer{}
+	broadcastServer.On("Deliver", mock.Anything).Run(func(args mock.Arguments) {
+		stream := args.Get(0).(orderer.AtomicBroadcast_DeliverServer)
+		actualEnv, err := stream.Recv()
+		assert.NoError(t, err)
+
+		expectedEnv, err := utils.CreateSignedEnvelopeWithTLSBinding(
+			common.HeaderType_DELIVER_SEEK_INFO,
+			channelID,
+			signer,
+			&orderer.SeekInfo{
+				Start:         &orderer.SeekPosition{Type: &orderer.SeekPosition_Specified{Specified: &orderer.SeekSpecified{Number: 2}}},
+				Stop:          &orderer.SeekPosition{Type: &orderer.SeekPosition_Specified{Specified: &orderer.SeekSpecified{Number: math.MaxUint64}}},
+				Behavior:      orderer.SeekInfo_BLOCK_UNTIL_READY,
+				ErrorResponse: orderer.SeekInfo_BEST_EFFORT,
+			},
+			int32(0),
+			uint64(0),
+			util.ComputeSHA256(tlsCert),
+		)
+		assert.NoError(t, err)
+		assert.True(t, proto.Equal(expectedEnv, actualEnv))
+
+		for _, blockResponse := range blockResponses {
+			stream.Send(&orderer.DeliverResponse{
+				Type: &orderer.DeliverResponse_Block{
+					Block: blockResponse,
+				},
+			})
+		}
+	}).Return(nil)
+
+	orderer.RegisterAtomicBroadcastServer(srv.Server(), broadcastServer)
+	go srv.Start()
+	defer srv.Stop()
+
+	_, port, err := net.SplitHostPort(lis.Addr().String())
+	assert.NoError(t, err)
+
+	puller := &BlocksStreamPuller{
+		RetryTimeout:        time.Millisecond,
+		FetchTimeout:        5 * time.Second,
+		MaxPullBlockRetries: 3,
+		Ledger:              ledger,
+		Dialer:              &testDialer{},
+		BlockVerifier:       blockVerifier,
+		Signer:              signer,
+		StreamCreator:       NewImpatientStream,
+		LastBlock:           lastBlock,
+		Logger:              flogging.MustGetLogger("test"),
+		Channel:             channelID,
+		TLSCert:             tlsCert,
+		Endpoints: []cluster.EndpointCriteria{
+			{
+				Endpoint:   fmt.Sprintf("127.0.0.1:%s", port),
+				TLSRootCAs: [][]byte{},
+			},
+		},
+	}
+
+	blockSeq := 0
+	ledger.On("Append", mock.Anything).Run(func(args mock.Arguments) {
+		defer wg.Done()
+		block := args.Get(0).(*common.Block)
+		assert.True(t, proto.Equal(blockResponses[blockSeq], block))
+		blockSeq++
+	}).Return(nil)
+
+	go puller.ContinuouslyPullBlocks()
+	wg.Wait()
+	puller.Stop()
+}
+
+func prepareBlocks(lastBlock *common.Block, signatures []*common.MetadataSignature, lastIndex uint64) []*common.Block {
+	var blocks []*common.Block
+
+	for prevBlock, i := lastBlock, lastBlock.Header.Number+1; i <= lastIndex; i++ {
+		block := common.NewBlock(i, prevBlock.Header.Hash())
+		block.Metadata.Metadata[common.BlockMetadataIndex_SIGNATURES] = utils.MarshalOrPanic(&common.Metadata{
+			Signatures: signatures,
+			Value: utils.MarshalOrPanic(&common.OrdererBlockMetadata{
+				LastConfig: &common.LastConfig{
+					Index: 1,
+				},
+			}),
+		})
+
+		prevBlock = block
+		blocks = append(blocks, block)
+	}
+
+	return blocks
+}
