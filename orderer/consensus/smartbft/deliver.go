eae9e16f12272ab605eb1ad6fbce7ed0fdb0369e
Yacov Manevich <yacovm@il.ibm.com>
2021-06-14 14:18:47 +0300

    Detect censorship when replicating from orderers
    Change-Id: I085097f9f0288685e72b5bb0250901a30eb91621
    Signed-off-by: Yacov Manevich <yacovm@il.ibm.com>

--- orderer/consensus/smartbft/deliver.go
+++ orderer/consensus/smartbft/deliver.go
@@ -33,6 +33,8 @@ type BlocksStreamPuller struct {
 	RetryTimeout      time.Duration
 	FetchTimeout      time.Duration
 
+	headerPullers []*headerPuller
+
 	Endpoints []cluster.EndpointCriteria
 	LastBlock *common.Block
 	TLSCert   []byte
@@ -48,6 +50,7 @@ type BlocksStreamPuller struct {
 	lock                 sync.RWMutex
 	connectionLock       sync.RWMutex
 	blockDeliverEndpoint cluster.EndpointCriteria
+	blockDeliverIndex    int
 	conn                 *grpc.ClientConn
 	stream               orderer.AtomicBroadcast_DeliverClient
 	stopFlag             int32
@@ -118,11 +121,59 @@ func (p *BlocksStreamPuller) Initialize(endpoints []cluster.EndpointCriteria, la
 	atomic.StoreInt32(&p.stopFlag, 0)
 }
 
+func (p *BlocksStreamPuller) allocateHeaderPullers() {
+	p.lock.RLock()
+	endpoints := p.Endpoints
+	blockDeliverIndex := p.blockDeliverIndex
+	p.lock.RUnlock()
+
+	for i, endpoint := range endpoints {
+		if i == blockDeliverIndex {
+			continue
+		}
+
+		logger := p.Logger.With("endpoint", endpoint.Endpoint)
+
+		hp := &headerPuller{
+			blockVerifier:     p.BlockVerifier,
+			committeeSize:     p.CommitteeSize,
+			lastCommitteeSize: p.lastCommitteeSize,
+			lastPulledSeq:     p.LastBlock.Header.Number,
+			channel:           p.Channel,
+			Signer:            p.Signer,
+			tlsCert:           p.TLSCert,
+			logger:            logger,
+			retryInterval:     p.RetryTimeout,
+			endpoint:          endpoint,
+			dialer:            p.Dialer,
+		}
+		p.headerPullers = append(p.headerPullers, hp)
+		hp.start()
+	}
+}
+
+func (p *BlocksStreamPuller) detectCensorship(lastPulledSeq uint64) bool {
+	n := len(p.headerPullers) + 1
+	f := (n - 1) / 3
+
+	var passedCount int
+
+	p.Logger.Debugf("Our last pulled block sequence is %d", lastPulledSeq)
+	for _, hp := range p.headerPullers {
+		if hp.passedSeq(lastPulledSeq) {
+			passedCount++
+		}
+	}
+
+	return passedCount > f
+}
+
 // assignEndpoint assign endpoints to decide where to fetch blocks from
 func (p *BlocksStreamPuller) assignEndpoint() {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
-	p.blockDeliverEndpoint = p.Endpoints[rand.Intn(len(p.Endpoints))]
+	p.blockDeliverIndex = rand.Intn(len(p.Endpoints))
+	p.blockDeliverEndpoint = p.Endpoints[p.blockDeliverIndex]
 	p.Logger.Infof("Will pull from %s", p.blockDeliverEndpoint.Endpoint)
 }
 
@@ -137,8 +188,33 @@ func (p *BlocksStreamPuller) tryFetchBlocks() (*common.Block, error) {
 			p.Logger.Errorf("failed sending seek envelope, error %s, endpoint %s", err, p.blockDeliverEndpoint)
 			return nil, err
 		}
+
+		p.allocateHeaderPullers()
 	}
+
+	noCensorship := make(chan struct{})
+
+	go func() {
+		for {
+			timer := time.NewTimer(time.Second * 5)
+
+			select {
+			case <-timer.C:
+				if p.detectCensorship(p.LastBlock.Header.Number) {
+					p.Logger.Warnf("Detected censorship")
+					p.abortStream()
+					timer.Stop()
+					return
+				}
+			case <-noCensorship:
+				timer.Stop()
+				return
+			}
+		}
+	}()
+
 	resp, err := p.stream.Recv()
+	close(noCensorship)
 	if err != nil {
 		p.Logger.Errorf("Failed receiving next block from %s: %v", p.blockDeliverEndpoint, err)
 		return nil, errors.Errorf("failed to receive next block, due to %s", err)
@@ -226,6 +302,12 @@ func (p *BlocksStreamPuller) disconnect() {
 		p.conn.Close()
 	}
 	p.conn = nil
+
+	for _, hp := range p.headerPullers {
+		hp.stop()
+	}
+
+	p.headerPullers = nil
 }
 
 func (p *BlocksStreamPuller) openStream() error {
@@ -305,4 +387,238 @@ func (p *BlocksStreamPuller) disconnected() bool {
 	defer p.connectionLock.RUnlock()
 
 	return p.conn == nil
+}
+
+type headerPuller struct {
+	// private
+	running     sync.WaitGroup
+	lock        sync.RWMutex
+	closeChan   chan struct{}
+	abortStream func()
+	conn        *grpc.ClientConn
+	// external
+	committeeSize     func() int
+	blockVerifier     BlockVerifier
+	lastCommitteeSize int
+	lastPulledSeq     uint64
+	channel           string
+	Signer            crypto.LocalSigner
+	tlsCert           []byte
+	logger            *flogging.FabricLogger
+	retryInterval     time.Duration
+	endpoint          cluster.EndpointCriteria
+	dialer            cluster.Dialer
+}
+
+func (hp *headerPuller) stop() {
+	defer hp.running.Wait()
+
+	hp.lock.Lock()
+
+	select {
+	case <-hp.closeChan:
+		hp.lock.Unlock()
+		return
+	default:
+		close(hp.closeChan)
+	}
+
+	hp.lock.Unlock()
+
+	hp.disconnect()
+
+}
+
+func (hp *headerPuller) start() {
+	hp.closeChan = make(chan struct{})
+	hp.running.Add(1)
+	go func() {
+		defer hp.running.Done()
+		hp.run()
+	}()
+}
+
+func (hp *headerPuller) closed() bool {
+	hp.lock.Lock()
+	defer hp.lock.Unlock()
+
+	select {
+	case <-hp.closeChan:
+		return true
+	default:
+		return false
+	}
+}
+
+func (hp *headerPuller) sleep() {
+	select {
+	case <-hp.closeChan:
+		return
+	case <-time.After(hp.retryInterval):
+	}
+}
+
+func (hp *headerPuller) disconnect() {
+	hp.lock.Lock()
+	defer hp.lock.Unlock()
+
+	if hp.abortStream != nil {
+		hp.abortStream()
+	}
+	if hp.conn != nil {
+		hp.conn.Close()
+	}
+
+	hp.conn = nil
+	hp.abortStream = nil
+}
+
+func (hp *headerPuller) run() {
+	for !hp.closed() {
+		var err error
+		hp.conn, err = hp.dialer.Dial(hp.endpoint)
+		if err != nil {
+			hp.logger.Warnf("Failed connecting to %s: %v", hp.endpoint.Endpoint, err)
+			hp.sleep()
+			continue
+		}
+
+		stream, err := hp.acquireStream()
+		if err != nil {
+			hp.disconnect()
+			hp.logger.Warnf("Failed acquiring stream to %s: %v", hp.endpoint.Endpoint, err)
+			hp.sleep()
+			continue
+		}
+
+		if err := hp.requestBlocks(stream); err != nil {
+			hp.disconnect()
+			hp.logger.Warnf("Failed requesting block headers from %s: %v", hp.endpoint.Endpoint, err)
+			continue
+		}
+
+		hp.pullHeaders(stream)
+
+	}
+
+}
+
+func (hp *headerPuller) pullHeaders(stream orderer.AtomicBroadcast_DeliverClient) {
+	for !hp.closed() {
+		resp, err := stream.Recv()
+		if err != nil {
+			hp.logger.Warnf("Failed pulling block headers from %s: %v", hp.endpoint.Endpoint, err)
+			hp.disconnect()
+			hp.sleep()
+			return
+		}
+
+		block := resp.GetBlock()
+		if block == nil {
+			hp.logger.Warnf("Deliver response we got from %s does not contain a block", hp.endpoint.Endpoint)
+			hp.stop()
+			return
+		}
+
+		if err := hp.verifyHeader(block); err != nil {
+			hp.logger.Warnf("Failed verifying block header received from %s: %v", hp.endpoint.Endpoint, err)
+			hp.sleep()
+			hp.disconnect()
+			return
+		}
+
+		hp.lock.Lock()
+		hp.lastPulledSeq = block.Header.Number
+		hp.lock.Unlock()
+	}
+}
+
+func (hp *headerPuller) verifyHeader(block *common.Block) error {
+	if block.Header == nil {
+		return errors.Errorf("block header is nil")
+	}
+	if hp.lastPulledSeq+1 != block.Header.Number {
+		return errors.Errorf("got unexpected sequence from %s - (%d) instead of (%d)", hp.endpoint.Endpoint, block.Header.Number, hp.lastPulledSeq+1)
+	}
+
+	md, err := types.CommitteeMetadataFromBlock(block)
+	if err != nil {
+		return errors.Wrapf(err, "failed extracting committee metadata from block %d", block.Header.Number)
+	}
+
+	committeeSize := hp.lastCommitteeSize
+
+	if committeeSize == 0 {
+		committeeSize = hp.committeeSize()
+	}
+
+	// sending nil for config envelope parameter, it will make use of the recent active configuration
+	if err := cluster.VerifyBlockSignature(block, hp.blockVerifier, nil, committeeSize); err != nil {
+		hp.logger.Warnf("Failed verifying block signature of block %d received from %s: %v", block.Header.Number, hp.endpoint.Endpoint, err)
+		return err
+	}
+
+	defer func() {
+		if md != nil {
+			hp.lastCommitteeSize = int(md.CommitteeSize)
+		}
+	}()
+
+	return nil
+}
+
+func (hp *headerPuller) passedSeq(lastPulledSeq uint64) bool {
+	hp.lock.RLock()
+	defer hp.lock.RUnlock()
+
+	hp.logger.Debugf("Endpoint %s is at height %d", hp.endpoint.Endpoint, hp.lastPulledSeq)
+
+	return hp.lastPulledSeq > lastPulledSeq
+}
+
+func (hp *headerPuller) acquireStream() (orderer.AtomicBroadcast_DeliverClient, error) {
+	abc := orderer.NewAtomicBroadcastClient(hp.conn)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	stream, err := abc.Deliver(ctx)
+	if err != nil {
+		cancel()
+		return nil, err
+	}
+
+	hp.lock.Lock()
+	defer hp.lock.Unlock()
+
+	once := &sync.Once{}
+	hp.abortStream = func() {
+		once.Do(func() {
+			cancel()
+		})
+	}
+
+	return stream, nil
+}
+
+func (hp *headerPuller) requestBlocks(stream orderer.AtomicBroadcast_DeliverClient) error {
+	env, err := utils.CreateSignedEnvelopeWithTLSBinding(
+		common.HeaderType_DELIVER_SEEK_INFO,
+		hp.channel,
+		hp.Signer,
+		&orderer.SeekInfo{
+			Start:         &orderer.SeekPosition{Type: &orderer.SeekPosition_Specified{Specified: &orderer.SeekSpecified{Number: hp.lastPulledSeq + 1}}},
+			Stop:          &orderer.SeekPosition{Type: &orderer.SeekPosition_Specified{Specified: &orderer.SeekSpecified{Number: math.MaxUint64}}},
+			Behavior:      orderer.SeekInfo_BLOCK_UNTIL_READY,
+			ErrorResponse: orderer.SeekInfo_BEST_EFFORT,
+			ContentType:   orderer.SeekInfo_HEADER_WITH_SIG,
+		},
+		int32(0),
+		uint64(0),
+		util.ComputeSHA256(hp.tlsCert),
+	)
+	if err != nil {
+		hp.logger.Errorf("failed to create seek envelope to start fetching blocks, endpoint %s", err, hp.endpoint)
+		return err
+	}
+	hp.logger.Debugf("sending request to pull block headers from %s", hp.endpoint.Endpoint)
+	return stream.Send(env)
 }
